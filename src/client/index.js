d4c26c5ae59818426fbd46de639b5ff4a25f5bec
Anton Nikitenko <ant.nikitenko@gmail.com>
2019-01-21 17:49:09 -0800

    Add canvas rect object for test

--- src/client/index.js
+++ src/client/index.js
@@ -20,7 +20,8 @@ class Course {
   }
 
   mouse_inside(x, y){
-    console.log(x+", "+y);
+    console.log("Shape x: "+this.x+"->"+(this.x+this.width));
+    console.log("Shape y: "+this.y+"->"+(this.y+this.width));
     var result = (this.x <= x) && (this.x + this.width >= x) &&
                  (this.y <= y) && (this.y + this.height >= y);
     return result;
@@ -40,23 +41,39 @@ class CanvasState {
     this.valid = false;
     this.courses_list = [];
 
+    var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
+    if (document.defaultView && document.defaultView.getComputedStyle) {
+      this.stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
+      this.stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
+      this.styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
+      this.styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
+    }
+    // Some pages have fixed-position bars (like the stumbleupon bar) at the top or left of the page
+    // They will mess up mouse coordinates and this fixes that
+    var html = document.body.parentNode;
+    this.htmlTop = html.offsetTop;
+    this.htmlLeft = html.offsetLeft;
+
+
     var canvas_state = this;
 
     this.canvas.addEventListener("mousedown", function(e){
-      if(canvas_state.courses_list[0].mouse_inside(e.pageX, e.pageY)){
-        console.log("inside");
-      }else{
-        console.log("outside");
+      var mouse_coordinates = canvas_state.getMouseCoordinates(e);
+      if(canvas_state.courses_list[0].mouse_inside(mouse_coordinates.x, mouse_coordinates.y)){
+        canvas_state.dragoffx = mouse_coordinates.x - canvas_state.courses_list[0].x;
+        canvas_state.dragoffy = mouse_coordinates.y - canvas_state.courses_list[0].y;
+        canvas_state.dragging = true;
+        canvas_state.valid = false;
+        console.log("INSIDE!!!");
       }
-      canvas_state.dragging = true;
-      canvas_state.valid = false;
     }, true);
 
     this.canvas.addEventListener("mousemove", function(e){
+      var mouse_coordinates = canvas_state.getMouseCoordinates(e);
       if(canvas_state.dragging){
-        var x = e.pageX;
-        var y = e.pageY;
-        canvas_state.refresh(x, y);
+
+        //var mouse_coordinates = canvas_state.getMouseCoordinates(e);
+        //canvas_state.refresh(mouse_coordinates.x, mouse_coordinates.y);
       }
     }, true);
 
@@ -73,7 +90,26 @@ class CanvasState {
   refresh(x, y){
     this.courses_list[0].draw(this.ctx, x, y);
   }
+
+  getMouseCoordinates(e){
+    // GIVE CREDIT https://github.com/simonsarris/Canvas-tutorials/blob/master/shapes.js
+    var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
+
+    if(element.offsetParent !== undefined){
+      do {
+        offsetX+=element.offsetLeft;
+        offsetY+=element.offsetTop;
+      }while((element = element.offsetParent));
+    }
+
+    offsetX+=this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
+    offsetY+=this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
+
+   mx = e.pageX - offsetX;
+   my = e.pageY - offsetY;
+   return {x: mx, y: my};
+  }
 }
 
 var canvas_state = new CanvasState(document.querySelector(canvas_id));
-canvas_state.addCourse(50, 30);
+canvas_state.addCourse(0, 0);
