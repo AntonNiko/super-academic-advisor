81a8206ea9cdd74d4f57ce9146609ac117646761
Anton Nikitenko <ant.nikitenko@gmail.com>
2019-02-10 16:10:54 -0800

    Added all course info to json file, and parsing json for program

--- html/assets/courses.js
+++ html/assets/courses.js
@@ -194,42 +194,6 @@ class ProgramSelection {
    }
 }
 
-var courses_eng_seng = {
-  "CSC 111": new Course("CSC","111",1.5,["F","Sp"],[],[]),
-  "ENGR 130": new Course("ENGR","130",0.5,["F","Sp"],[],[]),
-  "ENGR 110": new Course("ENGR","110",2.5,["F"],[],[]),
-  "MATH 100": new Course("MATH","100",1.5,["F","Sp","Su"],[],[]),
-  "MATH 109": new Course("MATH","109",1.5,["F","Sp","Su"],[],[]),
-  "MATH 110": new Course("MATH","110",1.5,["F"],[],[]),
-  "PHYS 110": new Course("PHYS","110",1.5,["F","Sp"],[],[]),
-  "CSC 115": new Course("CSC","115",1.5,["Sp","Su","F"],[[["CSC 110","p"],["CSC 111","p"]]],[]),
-  "ENGR 120": new Course("ENGR","120",2.5,["Sp"],[[["CSC 110","p"],["CSC 111","c"]], [["ENGR 110","p"],[["ENGR 112","ENGL 135"],"p"]]],[]),
-  "ENGR 141": new Course("ENGR","141",1.5,["Sp","Su"],[[["MATH 100","p"],["MATH 109","p"]],[["MATH 110","c"],["MATH 211","c"]]],[]),
-  "MATH 101": new Course("MATH","101",1.5,["Sp","Su","F"],[[["MATH 100","p"],["MATH 109","p"]]],[]),
-  "PHYS 111": new Course("PHYS","111",1.5,["Sp","Su"],[[["MATH 100","c"],["MATH 109","c"]],[["PHYS 110","p"]]],[]),
-  "CSC 230": new Course("CSC","230",1.5,["F","Sp","Su"],[[["CSC 115","p"],["CSC 116","p"]]],[]),
-  "CHEM 101": new Course("CHEM","101",1.5,["F","Su"],[],[],[]),
-  "ECE 260": new Course("ECE","260",1.5,["F","Su"],[[["MATH 101","p"]], [["MATH 110","p"],["MATH 211","c"]]],[]),
-  //"MATH 122": new Course("MATH","122",1.5,["F","Sp","Su"],[["MATH 100","MATH 109"]],[],[]),
-  //"SENG 265": new Course("SENG","265",1.5,["F","Sp","Su"],[["CSC 115","CSC 116"]],[],[]),
-  /*"STAT 260": new Course("STAT","260",1.5,["F","Sp","Su"],[["MATH 101"]],[],[])
-  "CSC 225": new Course("CSC","255",["CSC 115", "MATH 122"],[],[], false),
-  "ECE 310": new Course("ECE","310",["ECE 260"],[],[], false),
-  "ECON 180": new Course("ECON","180",["MATH 101"],[],[], false),
-  "SENG 275": new Course("SENG","275",["SENG 265"],[],[], false),
-  "CSC 361": new Course("CSC","361",["SENG 265", "CSC 230", "CSC 226"],[],[], false),
-  "CSC 226": new Course("CSC","226",["CSC 225"],[],[], false),
-  "ECE 360": new Course("ECE","360",["ECE 260"],[],[], false),
-  "SENG 321": new Course("SENG","321",["SENG 265"],[],[], false),
-  "SENG 371": new Course("SENG","371",["SENG 275"],[],[], false),
-  "CSC 355": new Course("CSC","355",["CSC 230", "MATH 122"],[],[], false),
-  "CSC 320": new Course("CSC","320",["CSC 226"],[],[], false),
-  "CSC 360": new Course("CSC","360",["SENG 265", "CSC 230", "CSC 226"],[],[], false),
-  "CSC 370": new Course("CSC","370",["CSC 226", "SENG 265"],[],[], false),
-  "SENG 350": new Course("SENG","350",["SENG 275"],[],[], false),
-  "SENG 360": new Course("SENG","360",["SENG 265"],[],[], false)*/
-}
-
 var program_sequence_seng_rec = {
   "1A":[["CSC 111","ENGR 130","ENGR 110","MATH 100","MATH 110","PHYS 110"],2018,"F"],
   "1B":[["CSC 115","ENGR 120","ENGR 141","MATH 101","PHYS 111"],2019,"Sp"],
