16be3797ac8936823085e02b8624c057b3ba7bcc
Anton <ant.nikitenko@gmail.com>
2019-03-22 13:12:39 -0700

    Changes to server directory, update .gitignore

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = 4000;
+const user_data = require("./user_info.json");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const db_str = "mongodb+srv://"+ user_data.username +":"+ user_data.password +"@saadata-qodey.mongodb.net/test?";
+mongoose.connect(db_str, { useNewUrlParser: true});
+const connection = mongoose.connection;
 
 app.listen(PORT, function(){
     console.log("Server is running on Port: "+PORT)
