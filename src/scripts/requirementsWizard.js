19f4ebf12601005bde540ae87f4bfac241ec1de1
Anton <ant.nikitenko@gmail.com>
2019-03-27 12:47:45 -0700

    Added code and methods to allow dynamic generation of a list of requirements

--- src/scripts/requirementsWizard.js
+++ src/scripts/requirementsWizard.js
@@ -22,18 +22,6 @@ class RequirementsWizard {
     // specializations, which are program-specific. Stores result in class
     generateCourseRequirementsBasedOnSelectedRequirements() {
         /*
-        Challenge:
-          Program requirements are composed of individual requirements. These individual
-          requirements are defined in several ways:
-           1) [SUBJ XXX] AND [SUBJ YYY]  (And conditional)
-           2) [SUBJ XXX] OR [SUBJ YYY] (Or conditional)
-           3) N of ([SUBJ 111],[SUBJ 222],...) (Exact number N of specific collection of courses)
-           4) >=N of ([SUBJ 111],[SUBJ 222],...) (Bounds of N number of courses collection)
-           5) [ELECTIVE CAT] (Elective course, based on specific category (CAT))
-           6) [CRED/N [SUBJ1 OR/AND SUBJ2 ...]  LEVEL WITH CRED_SUB LEVEL] (CRED credits or N courses with minimum of SUBJ(s), and optional LEVEL (i.e 100/200/300/400))
-        To implement first: 1), 2), 3), 4), 5)
-
-
         FOR ANY PROGRAM, MINOR, AND SPECIALIZATION CHOICE:
           1) ALL PROGRAM BASE REQUIREMENTS ARE SAME
           2) MINORS WILL SIMPLY ADD ON REQUIRED COURSES
@@ -60,7 +48,7 @@ class RequirementsWizard {
         // Handle minors
         if(this.selected_minor != "None") {
             var minor_requirements = this.requirements_data["Minors"][this.selected_minor];
-            this.result_course_requirements.push(minor_requirements);
+            this.result_course_requirements = this.result_course_requirements.concat(minor_requirements);
         }
     }
 
@@ -96,9 +84,9 @@ class RequirementsWizard {
       /*
       Statement types:
       1) ["SUBJ XXX","OR/AND","SUBJ YYY"] / [[],"OR/AND",[]] (Conditional)
-      2) [">=/</N OF",["SUBJ XXX","SUBJ YYY",...]] (Conditional collection)
+      2) [">=/</N OF",["SUBJ XXX","SUBJ YYY",...]] (Collection)
       3) [ELECTIVE CAT] (Elective)
-      4) ["N CREDITS/COURSES",["SUBJ"],["100",...],["WITH"],"M CREDITS/COURSES",["100",..]]] (Credit/course quantified)
+      4) ["N CREDITS/COURSES",["SUBJ"],["100",...],["WITH"],"M CREDITS/COURSES",["100",..]]] (Quantified)
       */
 
       if (requirement.length == 3 && ["OR","AND"].includes(requirement[1])) {
@@ -125,32 +113,81 @@ class RequirementsWizard {
     }
 
     getConditionalRequirementElement(requirement) {
-      console.log(requirement);
       var requirement_list = [];
 
-
       // Get jsx element of first element
-
+      requirement_list = requirement_list.concat(this.getRequirementElement(requirement[0]));
       // Add conitional element
       requirement_list.push(<li class="reqs-course-conditional"><span class="reqs-conditional-text">{requirement[1]}</span></li>)
-
       // Get jsx element of third element
+      requirement_list = requirement_list.concat(this.getRequirementElement(requirement[2]));
+
+      return requirement_list;
     }
 
     getCollectionRequirementElement(requirement) {
-      console.log(requirement);
+      var requirement_list = [];
+
+      // Handle first part of requirement (i.e >=/ N of...)
+      if(requirement[0].match(/>=\d+/)) {
+        var collection_limit = parseInt(requirement[0].match(/\d+/));
+        requirement_list.push(<li class="reqs-course-conditional"><span class="reqs-conditional-text">{collection_limit} OR MORE OF:</span></li>);
+      } else if (requirement[0].match(/\d+/)) {
+        var collection_number = parseInt(requirement[0].match(/\d+/));
+        requirement_list.push(<li class="reqs-course-conditional"><span class="reqs-conditional-text">{collection_number} OF:</span></li>);
+      }
+
+      // Get requirement element for all elements of second part of requirement 
+      for(var i=0; i<requirement[1].length; i++) {
+        requirement_list = requirement_list.concat(this.getRequirementElement(requirement[1][i]));
+      }
+
+      return requirement_list;
     }
 
     getElectiveRequirementElement(requirement) {
-      console.log(requirement);
+      var elective_type = requirement[0].slice(9);
+      var elective_abbreviation = this.requirements_data["Electives"][elective_type]["Abbreviation"];
+
+      return <li class="reqs-course-item"><span class="reqs-checkmark-bg"><img src=""></img></span><span class="reqs-course-name">{elective_abbreviation}</span></li>;
     }
 
     getQuantifiedRequirementElement(requirement) {
-      console.log(requirement);
+      var requirement_list = [];
+
+      var primary_requirement_type = requirement[0].match(/CREDITS/) != null ? "CREDITS" : "COURSES";
+      var requirement_number = parseInt(requirement[0].match(/\d+/));
+
+      // TODO: MANAGE SECOND PART OF REQUIREMENT OF QUANTIFIED REQUIREMENT
+
+      return (
+        <li class="reqs-course-item">
+          <span class="reqs-checkmark-bg">
+            <img src=""></img></span><span class="reqs-course-name">{requirement_number} {primary_requirement_type}
+          </span>
+        </li>
+      );
     }
 
     getCourseRequirementElement(requirement) {
       return <li class="reqs-course-item"><span class="reqs-checkmark-bg"><img src=""></img></span><span class="reqs-course-name">{requirement[0]}</span></li>;
+    }
+
+    getRequirementElement(requirement) {
+      switch(this.getRequirementType(requirement)) {
+        case "conditional":
+          return this.getConditionalRequirementElement(requirement);
+        case "collection":
+          return this.getCollectionRequirementElement(requirement);
+        case "elective":
+          return this.getElectiveRequirementElement(requirement);
+        case "quantified":
+          return this.getQuantifiedRequirementElement(requirement);
+        case "course":
+          return this.getCourseRequirementElement(requirement);
+        default:
+          return null;
+      }      
     }
 
     // This method is called from Requirements component, and is used to convert the
@@ -159,27 +196,7 @@ class RequirementsWizard {
       var requirements_list = [];
 
       for(var i=0; i<this.result_course_requirements.length; i++) {
-        var requirement = this.result_course_requirements[i];
-        switch(this.getRequirementType(requirement)) {
-          case "conditional":
-            this.getConditionalRequirementElement(requirement);
-            break;
-          case "collection":
-            this.getCollectionRequirementElement(requirement);
-            break;
-          case "elective":
-            this.getElectiveRequirementElement(requirement);
-            break;
-          case "quantified":
-            this.getQuantifiedRequirementElement(requirement);
-            break;
-          case "course":
-            this.getCourseRequirementElement(requirement);
-            break;
-          default:
-            break;
-        }
-
+        requirements_list = requirements_list.concat(this.getRequirementElement(this.result_course_requirements[i]));
         requirements_list.push(<li class="reqs-course-separator"><hr></hr></li>);
       }
       /*
